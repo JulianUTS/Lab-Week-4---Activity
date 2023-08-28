9eee14f467305cb383df457fa6ffe673d5dd414f
JulianUTS <julian.r.arceo@student.uts.edu.au>
2023-08-28 20:08:49 +1000

    70% V1

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,10 +6,15 @@ public class LoadAssets : MonoBehaviour
 {
 
 public GameObject redObj;
+[SerializeField] private GameObject blueObj;
 
     // Start is called before the first frame update
     void Start()
     {
+ Application.targetFrameRate = 60;
+
+Instantiate(redObj, new Vector3(2,0,0), new Quaternion(0,0,0,0));
+Instantiate(blueObj, new Vector3(-2,0,0), new Quaternion(0,0,0,0));
 
     }
 
